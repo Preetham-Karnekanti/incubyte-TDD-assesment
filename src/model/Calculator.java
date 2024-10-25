62a695bda331a19786d6f480bacdd7bce4ac9ed5
Preetham-Karnekanti <preethamsam7@gmail.com>
2024-10-25 19:54:40 +0530

    added ability to support custom delimiter

--- src/model/Calculator.java
+++ src/model/Calculator.java
@@ -4,10 +4,20 @@ import java.util.Arrays;
 
 public class Calculator {
 
-    public int add(String numbers){
-        if(numbers.isBlank())
+    public final String DEFAULT_DELIMiTER = "[,\n]";
+
+    public int add(String numbers) {
+        if (numbers.isBlank()) {
             return 0;
-        String[] parsedNumbers = numbers.split("[,\n]");
+        }
+        String delimiter = DEFAULT_DELIMiTER;
+        String numberString = numbers;
+        if (numbers.startsWith("//")) {
+            int delimiterIndex = numbers.indexOf("\n");
+            delimiter = numbers.substring(2, delimiterIndex);
+            numberString = numbers.substring(delimiterIndex + 1);
+        }
+        String[] parsedNumbers = numberString.split(delimiter);
         return Arrays.stream(parsedNumbers).map(String::strip).mapToInt(Integer::parseInt).sum();
     }
 }
--- src/test/CalculatorTest.java
+++ src/test/CalculatorTest.java
@@ -2,42 +2,51 @@ package test;
 
 import model.Calculator;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class CalculatorTest {
 
     private final Calculator calculator = new Calculator();
+
     @Test
-    public void shouldReturnZeroIfStringIsBlank(){
+    public void shouldReturnZeroIfStringIsBlank() {
         assertEquals(0, calculator.add(""));
     }
 
 
     @Test
-    public void shouldReturnOneNumberIfNumberIsGiven(){
+    public void shouldReturnOneNumberIfNumberIsGiven() {
         assertEquals(1, calculator.add("1"));
         assertEquals(1, calculator.add("1,"));
     }
 
     @Test
-    public void shouldAddAllNumbersSeparatedByComma(){
+    public void shouldAddAllNumbersSeparatedByComma() {
         assertEquals(10, calculator.add("1,2,3,4"));
         assertEquals(11, calculator.add("1,2,3,5"));
     }
 
     @Test
-    public void shouldAddAllNumbersSeparatedNewLine(){
+    public void shouldAddAllNumbersSeparatedNewLine() {
         assertEquals(10, calculator.add("1\n2\n3\n4\n"));
     }
 
     @Test
-    public void shouldAddAllNumbersSeparatedNewLineAndComma(){
+    public void shouldAddAllNumbersSeparatedNewLineAndComma() {
         assertEquals(10, calculator.add("1,2\n3\n4\n"));
     }
 
     @Test
-    public void shouldNotThrowExceptionWhenSpaceIsPresent(){
+    public void shouldNotThrowExceptionWhenSpaceIsPresent() {
         assertDoesNotThrow(() -> calculator.add("1, 2 , 2"));
+    }
+
+    @Test
+    public void shouldAbleToSupportDifferentDelimiter() {
+        assertEquals(6, calculator.add("//;\n1;2;3"));
+        assertEquals(6, calculator.add("//#\n1#2#3"));
+        assertEquals(6, calculator.add("//&\n1&2&3"));
     }
 }
