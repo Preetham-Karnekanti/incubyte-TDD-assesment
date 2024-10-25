cf78055ca24bb7fcc6e05c7526118f60aa08ea34
Preetham-Karnekanti <preethamsam7@gmail.com>
2024-10-25 14:46:32 +0530

    added ability add numbers in new line

--- src/model/Calculator.java
+++ src/model/Calculator.java
@@ -7,7 +7,7 @@ public class Calculator {
     public int add(String numbers){
         if(numbers.isBlank())
             return 0;
-        String[] parsedNumbers = numbers.split(",");
+        String[] parsedNumbers = numbers.split("[,\n]");
         return Arrays.stream(parsedNumbers).map(String::strip).mapToInt(Integer::parseInt).sum();
     }
 }
--- src/test/CalculatorTest.java
+++ src/test/CalculatorTest.java
@@ -1,29 +1,43 @@
 package test;
 
 import model.Calculator;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class CalculatorTest {
 
+    private final Calculator calculator = new Calculator();
     @Test
     public void shouldReturnZeroIfStringIsBlank(){
-        Calculator calculator = new Calculator();
-        Assert.assertEquals(0,  calculator.add(""));
+        assertEquals(0, calculator.add(""));
     }
 
 
     @Test
     public void shouldReturnOneNumberIfNumberIsGiven(){
-        Calculator calculator = new Calculator();
-        Assert.assertEquals(1, calculator.add("1"));
-        Assert.assertEquals(1, calculator.add("1,"));
+        assertEquals(1, calculator.add("1"));
+        assertEquals(1, calculator.add("1,"));
     }
 
     @Test
     public void shouldAddAllNumbersSeparatedByComma(){
-        Calculator calculator = new Calculator();
-        Assert.assertEquals(10, calculator.add("1,2,3,4"));
-        Assert.assertEquals(11, calculator.add("1,2,3,5"));
+        assertEquals(10, calculator.add("1,2,3,4"));
+        assertEquals(11, calculator.add("1,2,3,5"));
+    }
+
+    @Test
+    public void shouldAddAllNumbersSeparatedNewLine(){
+        assertEquals(10, calculator.add("1\n2\n3\n4\n"));
+    }
+
+    @Test
+    public void shouldAddAllNumbersSeparatedNewLineAndComma(){
+        assertEquals(10, calculator.add("1,2\n3\n4\n"));
+    }
+
+    @Test
+    public void shouldNotThrowExceptionWhenSpaceIsPresent(){
+        assertDoesNotThrow(() -> calculator.add("1, 2 , 2"));
     }
 }
